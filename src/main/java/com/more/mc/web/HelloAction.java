3958ac5520fc2c980bda399b4a2b45d0b848c902
wangqq <272334814@qq.com>
2019-07-22 16:57:56 +0800

    大富大贵

---
+++ src/main/java/com/more/mc/web/HelloAction.java
@@ -0,0 +1,16 @@
+/*
+ * @File HelloAction.java
+ * @Package com.more.mc.web
+ * @Copyinght Copyright (c) 2019
+ * @Company morelean
+ * @author Administrator
+ * @date 2019年7月22日 下午4:56:39
+ * @version V1.0
+ */
+
+package com.more.mc.web;
+
+public class HelloAction
+{
+
+}
